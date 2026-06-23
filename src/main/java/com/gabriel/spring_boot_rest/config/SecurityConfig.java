752abedf787280ec2005644120d30670215f3777
LeirbagAC <albuquerqueg373@gmail.com>
2026-06-23 11:58:05 -0300

    feature: Adicionei o CorsConfigurationSource

--- src/main/java/com/gabriel/spring_boot_rest/config/SecurityConfig.java
+++ src/main/java/com/gabriel/spring_boot_rest/config/SecurityConfig.java
@@ -15,6 +15,12 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -37,9 +43,10 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(request -> request
-                        .requestMatchers("/register", "/JobPosts" ,"/JobPost/search").permitAll()
+                        .requestMatchers("/register", "/login", "/JobPosts" ,"/JobPost/search").permitAll()
                         .anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults())
                 .sessionManagement(session ->
@@ -47,6 +54,19 @@ public class SecurityConfig {
                 );
 
         return http.build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
 }
--- src/main/java/com/gabriel/spring_boot_rest/controller/UserController.java
+++ src/main/java/com/gabriel/spring_boot_rest/controller/UserController.java
@@ -3,9 +3,7 @@ package com.gabriel.spring_boot_rest.controller;
 import com.gabriel.spring_boot_rest.model.User;
 import com.gabriel.spring_boot_rest.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserController {
@@ -16,5 +14,11 @@ public class UserController {
     @PostMapping("register")
     public User register(@RequestBody User user) {
         return userService.saveUser(user);
+    }
+
+    @GetMapping("login")
+    public User login(User user) {
+        String username = user.getUsername();
+        return userService.findByUsername(username);
     }
 }
