e72daa550c45a2a3701c23104ebe361aec2e411e
LeirbagAC <albuquerqueg373@gmail.com>
2026-06-23 15:15:13 -0300

    fix: Auto incremento do user_id no banco e adicionado para  encontrar user por nome

--- src/main/java/com/gabriel/spring_boot_rest/service/UserService.java
+++ src/main/java/com/gabriel/spring_boot_rest/service/UserService.java
@@ -23,4 +23,8 @@ public class UserService {
         return userRepo.save(user);
     }
 
+    public User findByUsername(String username) {
+        return userRepo.findByUsername(username);
+    }
+
 }
